19f1a1b444cdd35a5b5a69ba3e31101b256e5a3a
Paul Kim <kimbaudi@gmail.com>
2017-07-12 10:56:30 -0700

    refactor LayoutModule layoutRoutes. Improve NavigationComponent active nav highlight and subnav toggle.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,17 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  ActivatedRoute,
-  Router,
-  NavigationStart,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError,
-  RoutesRecognized,
-  RouteConfigLoadStart,
-  RouteConfigLoadEnd,
-  Event,
-  UrlSegment
-} from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd, Event, UrlSegment } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -25,24 +13,12 @@ export class LayoutComponent implements OnInit {
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
 
     router.events.subscribe((event: Event) => {
-      // if(event instanceof NavigationStart) {
-      // }
       if(event instanceof NavigationEnd) {
         // console.log(activatedRoute.firstChild.url['value'][0].path); // working
         // activatedRoute.firstChild.url.subscribe((url: UrlSegment[]) => console.log(url[0].path)); // working
         // console.log(activatedRoute.snapshot.firstChild.url[0].path); // working
         this.isFullscreen = this.fullscreenPaths.includes(activatedRoute.firstChild.url['value'][0].path) ? true : false;
       }
-      // if(event instanceof NavigationCancel) {
-      // }
-      // if(event instanceof NavigationError) {
-      // }
-      // if(event instanceof RoutesRecognized) {
-      // }
-      // if(event instanceof RouteConfigLoadStart) {
-      // }
-      // if(event instanceof RouteConfigLoadEnd) {
-      // }
     });
 
   }
--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 import { SharedService } from "../../shared/services/shared.service";
 
@@ -22,31 +23,86 @@ import { SharedService } from "../../shared/services/shared.service";
     ]
 })
 export class NavigationComponent implements OnInit {
-    sidebarVisible: boolean;
+  sidebarVisible: boolean;
+  activatedNav: string;
 
-    // Sub menu visibilities
-    navigationSubState:any = {
-        Tables: 'inactive',
-        Forms: 'inactive',
-        SamplePages: 'inactive',
-        UserInterface: 'inactive',
-        Components: 'inactive',
-        Charts: 'inactive',
-    };
+  // Sub menu visibilities
+  navigationSubState:any = {
+    Tables: 'inactive',
+    Forms: 'inactive',
+    UserInterface: 'inactive',
+    Components: 'inactive',
+    Charts: 'inactive',
+    SamplePages: 'inactive',
+  };
 
-    // Toggle sub menu
-    toggleNavigationSub(menu, event) {
-        event.preventDefault();
-        this.navigationSubState[menu] = (this.navigationSubState[menu] === 'inactive' ? 'active' : 'inactive');
+  initActiveNavigation(path) {
+    switch(path) {
+      case 'home':
+        this.activatedNav = 'Home';
+        break;
+      case 'typography':
+        this.activatedNav = 'Typography';
+        break;
+      case 'widgets':
+        this.activatedNav = 'Widgets';
+        break;
+      case 'table':
+        this.activatedNav = 'Tables';
+        this.navigationSubState.Tables = 'active';
+        break;
+      case 'form':
+        this.activatedNav = 'Forms';
+        this.navigationSubState.Forms = 'active';
+        break;
+      case 'user-interface':
+        this.activatedNav = 'UserInterface';
+        this.navigationSubState.UserInterface = 'active';
+        break;
+      case 'components':
+        this.activatedNav = 'Components';
+        this.navigationSubState.Components = 'active';
+        break;
+      case 'charts':
+        this.activatedNav = 'Charts';
+        this.navigationSubState.Charts = 'active';
+        break;
+      case 'calendar':
+        this.activatedNav = 'Calendar';
+        break;
+      case 'sample-pages':
+        this.activatedNav = 'SamplePages';
+        this.navigationSubState.SamplePages = 'active';
+        break;
+    }
+  }
+
+  // Toggle sub menu
+  toggleNavigationSub(menu: string, event) {
+    event.preventDefault();
+
+    let parentIsActive:boolean = event.currentTarget.parentElement.className.split(' ').includes('active');
+    this.activatedNav = (this.activatedNav !== menu || parentIsActive) ? menu : '';
+
+    for (var key of Object.keys(this.navigationSubState)) {
+      if (key !== menu && this.navigationSubState[key] === 'active') {
+        this.navigationSubState[key] = 'inactive';
+      }
     }
 
-    constructor(private sharedService: SharedService) {
-        sharedService.sidebarVisibilitySubject.subscribe((value) => {
-            this.sidebarVisible = value
-        })
-    }
+    this.navigationSubState[menu] = (this.navigationSubState[menu] === 'inactive' ? 'active' : 'inactive');
+  }
 
-    ngOnInit() {
-    }
+  constructor(private activatedRoute: ActivatedRoute, private sharedService: SharedService) {
+    sharedService.sidebarVisibilitySubject.subscribe((value) => {
+      this.sidebarVisible = value
+    })
+
+    let path = activatedRoute.firstChild.url['value'][0].path;
+    this.initActiveNavigation(path);
+  }
+
+  ngOnInit() {
+  }
 
 }
